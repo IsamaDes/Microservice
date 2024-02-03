a456dfdbd4bbb31adab318a5bed37550c3207f11
Desmond Isama <145404612+IsamaDes@users.noreply.github.com>
2024-02-03 17:00:02 +0100

    Add files via upload

---
+++ Microservice/OrderMicroservice/src/main/java/com/example/ordermicroservice/OrderController.java
@@ -0,0 +1,14 @@
+package com.example.ordermicroservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello";
+    }
+}
